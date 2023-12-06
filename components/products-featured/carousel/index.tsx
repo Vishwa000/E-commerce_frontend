45f9db895655b9e223dee2f5247976503f5feac4
vichuvishwa004@gmail.com <104341699+avinash-mediwave@users.noreply.github.com>
2023-12-06 11:22:53 +0530

    product api

--- components/products-featured/carousel/index.tsx
+++ components/products-featured/carousel/index.tsx
@@ -1,58 +1,82 @@
-import ProductItem from './../../product-item';
-import { ProductTypeList } from 'types';
-
-// import Swiper core and required components
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import ProductsLoading from '../../products-content/list/loading';
+import ProductItem from './../../product-item';
 
-let slidesPerView = 1.3;
-let centeredSlides = true;
-let spaceBetween = 30;
-if (process.browser) {
-  if(window.innerWidth > 768) {
+const apiURL = 'http://localhost:8000/api/products';
+
+const ProductsCarousel = () => {
+  const [data, setData] = useState<any[] | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiURL);
+        setData(response.data.data);
+      } catch (error) {
+        setError('Failed to load products');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <ProductsLoading />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  let slidesPerView = 1.3;
+  let centeredSlides = window.innerWidth <= 768;
+  let spaceBetween = 30;
+
+  if (window.innerWidth > 768) {
     slidesPerView = 3;
     spaceBetween = 35;
     centeredSlides = false;
   }
-  if(window.innerWidth > 1024) {
+
+  if (window.innerWidth > 1024) {
     slidesPerView = 4;
     spaceBetween = 65;
     centeredSlides = false;
   }
-}
-
-type ProductsCarouselType = {
-  products: ProductTypeList[]
-}
-
-const ProductsCarousel = ({ products }: ProductsCarouselType) => {
-  if (!products) return <div>Loading</div>;
 
   return (
     <div className="products-carousel">
-      <Swiper 
-      spaceBetween={spaceBetween} 
-      loop={true} 
-      centeredSlides={centeredSlides} 
-      watchOverflow={true} 
-      slidesPerView={slidesPerView} 
-      className="swiper-wrapper">
-        {products.map(item => (
-          <SwiperSlide key={item.id}>
-            <ProductItem 
-              id={item.id} 
-              name={item.name}
-              price={item.price}
-              color={item.color}
-              discount={item.discount}
-              currentPrice={item.currentPrice}
-              key={item.id}
-              images={item.images} 
-            />
-          </SwiperSlide>
-        ))}
+      <Swiper
+        spaceBetween={spaceBetween}
+        loop={true}
+        centeredSlides={centeredSlides}
+        watchOverflow={true}
+        slidesPerView={slidesPerView}
+        className="swiper-wrapper"
+      >
+        {Array.isArray(data) ? (
+          data.map((item) => (
+            <SwiperSlide key={item.id} className="products-list">
+              <ProductItem
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                key={item.id}
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <div>Data is not an array</div>
+        )}
       </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsCarousel
+export default ProductsCarousel;
